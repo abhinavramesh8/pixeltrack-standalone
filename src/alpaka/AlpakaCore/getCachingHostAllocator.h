5231ad373130015e840106fa709e9440e34dcb30
Abhinav Ramesh <abhinavramesh@utexas.edu>
2021-08-16 12:49:19 +0200

    [alpaka] Make getCachingHostAllocator utilize functions in CachingDeviceAllocator and getCachingDeviceAllocator.h

--- src/alpaka/AlpakaCore/getCachingHostAllocator.h
+++ src/alpaka/AlpakaCore/getCachingHostAllocator.h
@@ -5,47 +5,9 @@
 #include <iostream>
 
 #include "CachingHostAllocator.h"
-
-// #include "getCachingDeviceAllocator.h"
+#include "getCachingDeviceAllocator.h"
 
 namespace cms::alpaka::allocator {
-  // TODO : move this to CachingDeviceAllocator.h later
-  /**
-   * Integer pow function for unsigned base and exponent
-   */
-  unsigned int IntPow(unsigned int base, unsigned int exp) {
-    unsigned int retval = 1;
-    while (exp > 0) {
-      if (exp & 1) {
-        retval = retval * base;  // multiply the result by the current base
-      }
-      base = base * base;  // square the base
-      exp = exp >> 1;      // divide the exponent in half
-    }
-    return retval;
-  }
-
-  // TODO : move all the stuff below to getCachingDeviceAllocator.h later
-  constexpr unsigned int binGrowth = 2;
-  constexpr unsigned int minBin = 8;
-  constexpr unsigned int maxBin = 30;
-  constexpr size_t maxCachedBytes = 0;
-  constexpr double maxCachedFraction = 0.8;
-  constexpr bool debug = false;
-  inline size_t minCachedBytes() {
-    size_t ret = std::numeric_limits<size_t>::max();
-    const size_t numberOfDevices {::alpaka::getDevCount<ALPAKA_ACCELERATOR_NAMESPACE::PltfAcc1>()};
-    for (size_t i = 0; i < numberOfDevices; ++i) {
-      const auto device {::alpaka::getDevByIdx<ALPAKA_ACCELERATOR_NAMESPACE::DevAcc1>(i)};
-      const size_t freeMemory {::alpaka::getFreeMemBytes(device)};
-      ret = std::min(ret, static_cast<size_t>(maxCachedFraction * freeMemory));
-    }
-    if (maxCachedBytes > 0) {
-      ret = std::min(ret, maxCachedBytes);
-    }
-    return ret;
-  }
-
   template <class TData>
   inline CachingHostAllocator<TData>& getCachingHostAllocator() {
     if (debug) {
@@ -55,7 +17,7 @@ namespace cms::alpaka::allocator {
                 << "  max bin    " << maxBin << "\n"
                 << "  resulting bins:\n";
       for (auto bin = minBin; bin <= maxBin; ++bin) {
-        auto binSize = IntPow(binGrowth, bin);
+        auto binSize = CachingDeviceAllocator<TData>::IntPow(binGrowth, bin);
         if (binSize >= (1 << 30) and binSize % (1 << 30) == 0) {
           std::cout << "    " << std::setw(8) << (binSize >> 30) << " GB\n";
         } else if (binSize >= (1 << 20) and binSize % (1 << 20) == 0) {
